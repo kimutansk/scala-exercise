a80d32f75a8bf988cbbdce3a5eb224ac35c05f65
Kimura Sotaro <rfbringer@gmail.com>
2014-07-29 07:01:40 +0900

    Add local TextCountApp

---
+++ spark-example/src/main/scala/com/github/kimutansk/spark/example/TestCountApp.scala
@@ -0,0 +1,20 @@
+package com.github.kimutansk.spark.example
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+
+/**
+ * テキストファイルをRDDに読み込み、RDDに対して繰り返しテキストカウントを行うSparkサンプルアプリケーション
+ */
+object TextCountApp {
+  def main(args: Array[String]) {
+    val logFile = "C:/pom.xml"
+    val conf = new SparkConf().setAppName("TextCountApp").setMaster("local")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    val numSparks = logData.filter(line => line.contains("Spark")).count()
+    println("Lines with a: %s, Lines with b: %s, Lines with Spark: %s".format(numAs, numBs, numSparks))
+  }
+}
