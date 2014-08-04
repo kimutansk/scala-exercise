81c8bfa14b1576700775eacc96375c4c1713d46a
Kimura Sotaro <rfbringer@gmail.com>
2014-08-05 06:57:58 +0900

    Basic Config confirm code.

--- akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/routing/ConfiguredRoutingApp.scala
+++ akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/routing/ConfiguredRoutingApp.scala
@@ -1,9 +1,17 @@
 package com.github.kimutansk.akka.exercise.routing
 
+import akka.actor.{Props, ActorSystem}
+import com.github.kimutansk.akka.exercise.message.{ParentActor, ChildActor}
+import scala.collection.immutable
+import akka.routing.RoundRobinRoutingLogic
+
 
 /**
  * 設定ファイルからルーティング定義を読み込むサンプル
  */
-class ConfiguredRoutingApp {
-
+object ConfiguredRoutingApp extends App {
+  override def main(args: Array[String]): Unit = {
+    val system = ActorSystem.apply("MessageSendApp")
+    println(system.settings);
+  }
 }
