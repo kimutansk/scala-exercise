25596eebc87cb7ebe944c1067604e98b6aaba38e
Kimura Sotaro <rfbringer@gmail.com>
2014-08-23 12:10:36 +0900

    Programable Remote Deploy

--- akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/remote/RemoteRouterApp.scala
+++ akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/remote/RemoteRouterApp.scala
@@ -8,7 +8,7 @@ import akka.remote.RemoteScope
 /**
  * Akka-Remoteを用いて接続を受け付けるクラス
  */
-object RemoteClientApp extends App {
+object RemoteRouterApp extends App {
   override def main(args: Array[String]): Unit = {
     val config = ConfigFactory.load("conf/remote-client-app.conf")
     implicit val system = ActorSystem.apply("RemoteClientApp", config)
