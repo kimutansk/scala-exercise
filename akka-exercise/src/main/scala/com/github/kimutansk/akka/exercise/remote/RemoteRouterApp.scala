29309d3de9d93fa4bf4912cbdf0c5d6328ac3d12
Kimura Sotaro <rfbringer@gmail.com>
2014-08-25 06:09:55 +0900

    Remote router deploy developing

--- akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/remote/RemoteRouterApp.scala
+++ akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/remote/RemoteRouterApp.scala
@@ -10,35 +10,20 @@ import akka.remote.RemoteScope
  */
 object RemoteRouterApp extends App {
   override def main(args: Array[String]): Unit = {
-    val config = ConfigFactory.load("conf/remote-client-app.conf")
-    implicit val system = ActorSystem.apply("RemoteClientApp", config)
-    // Remote Lookup
-    val remoteActorRef = system.actorSelection("akka.tcp://RemoteServerApp@127.0.0.1:2552/user/Receive")
+    val config = ConfigFactory.load("conf/remote-router-app.conf")
+    implicit val system = ActorSystem.apply("RemoteRouterApp", config)
 
     val inbox = ActorDSL.inbox()
-    remoteActorRef.tell("Remote", inbox.getRef())
-
+    // RemoteRouterCreate
+    val remoteRouterRef = system.actorOf(Props[MessagePrintActor],
+      "remotePool")
+    remoteRouterRef.tell("RemoteRouter1", inbox.getRef())
+    remoteRouterRef.tell("RemoteRouter2", inbox.getRef())
+    remoteRouterRef.tell("RemoteRouter3", inbox.getRef())
+    remoteRouterRef.tell("RemoteRouter4", inbox.getRef())
     val received1 = inbox.receive()
     println("received1:" + received1)
 
-    // RemoteCreate
-    val remoteRouterRef = system.actorOf(Props[MessagePrintActor],
-      "router1")
-    remoteRouterRef.tell("RemoteRouter", inbox.getRef())
-    val received2 = inbox.receive()
-    println("received2:" + received2)
-
-    val remoteRouterSelectRef = system.actorSelection("akka.tcp://RemoteServerApp@127.0.0.1:2552/remote/akka.tcp/RemoteClientApp@127.0.0.1:2553/user/router1");
-    remoteRouterSelectRef.tell("RemoteSelection", inbox.getRef())
-    val received3 = inbox.receive()
-    println("received3:" + received3)
-
-    val remoteAddress = AddressFromURIString("akka.tcp://RemoteServerApp@127.0.0.1:2552")
-    val programRemoteRef = system.actorOf(Props[MessagePrintActor].
-      withDeploy(Deploy(scope = RemoteScope(remoteAddress))))
-    programRemoteRef.tell("RemoteRouter", inbox.getRef())
-    val received4 = inbox.receive()
-    println("received4:" + received4)
 
     Thread.sleep(30000)
     system.shutdown()
