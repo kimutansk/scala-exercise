0f909f566924585f8d844aff3ef758a82c8f1935
Kimura Sotaro <rfbringer@gmail.com>
2014-08-30 23:33:04 +0900

    Typed Actor minimum sample

--- akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/typed/CalculatorImpl.scala
+++ akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/typed/CalculatorImpl.scala
@@ -1,12 +1,11 @@
 package com.github.kimutansk.akka.exercise.typed
 
 import scala.concurrent.Future
-import akka.actor.Actor
 
 /**
  * Typed Actor検証用のActorコード
  */
-class CalculatorImpl(val id: String) extends Calculator with Actor {
+class CalculatorImpl(val id: String) extends Calculator {
   // Actorをextendsした場合、TypedActorofメソッドは使用できなくなる。
   // そのため、Actorがもともと持っていたContextやSenderは使用できなくなる。
   def this() = this("default")
@@ -20,11 +19,4 @@ class CalculatorImpl(val id: String) extends Calculator with Actor {
   def squareNow(i: Int): Int = i * i
 
   def squareTry(i: Int): Int = throw new Exception("Catch me!")
-
-  def receive = {
-    case anyMsg: Any =>{
-      val message = self.path + ": Received " + anyMsg
-      println(message);
-    }
-  }
-}
+ }
