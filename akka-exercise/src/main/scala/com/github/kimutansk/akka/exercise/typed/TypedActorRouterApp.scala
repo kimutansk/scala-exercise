159b35016e50a81cbd7f8117667fe4b5b8faafb9
Kimura Sotaro <rfbringer@gmail.com>
2014-09-10 07:15:11 +0900

    Typed Router Debug

--- akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/typed/TypedActorRouterApp.scala
+++ akka-exercise/src/main/scala/com/github/kimutansk/akka/exercise/typed/TypedActorRouterApp.scala
@@ -18,21 +18,25 @@ object TypedActorRouterApp extends App {
 
     val remoteAddress = AddressFromURIString("akka.tcp://TypedActorServerApp@127.0.0.1:2552")
 
-    val calculator:Calculator = TypedActor(system).typedActorOf(TypedProps[CalculatorImpl].
+    val calculator: Calculator = TypedActor(system).typedActorOf(TypedProps[CalculatorImpl].
       withDeploy(Deploy(scope = RemoteScope(remoteAddress))))
 
     val pathResult = calculator.pathNow("STR")
     println("pathResult:" + pathResult)
 
     val routees: List[Calculator] = List.fill(5)(createCalc(system, remoteAddress))
-    val routeePaths = routees map { r =>
-      TypedActor(system).getActorRefFor(r).path.toStringWithoutAddress
+    routees map {
+      r => r.pathNow("Test")
+    }
+    val routeePaths = routees map {
+      r =>
+        TypedActor(system).getActorRefFor(r).path.toStringWithoutAddress
     }
 
     val router: ActorRef = system.actorOf(RoundRobinGroup(routeePaths).props())
 
     val typedRouter: Calculator =
-      TypedActor(system).typedActorOf(TypedProps[CalculatorImpl], actorRef = router)
+      TypedActor(system).typedActorOf(TypedProps[CalculatorImpl](), actorRef = router)
 
     val pathResult1 = typedRouter.pathNow("STR1")
     println("pathResult1:" + pathResult1)
@@ -50,8 +54,8 @@ object TypedActorRouterApp extends App {
     system.shutdown()
   }
 
-  def createCalc(system: ActorSystem, address:Address): Calculator = {
-    val result:Calculator = TypedActor(system).typedActorOf(TypedProps[CalculatorImpl].withDeploy(Deploy(scope = RemoteScope(address))))
+  def createCalc(system: ActorSystem, address: Address): Calculator = {
+    val result: Calculator = TypedActor(system).typedActorOf(TypedProps[CalculatorImpl].withDeploy(Deploy(scope = RemoteScope(address))))
     return result
   }
 }
